82fcfd5c1cada9ab5df3cae1a2010d768ea15033
VSBANEES <30316990+VSBANEES@users.noreply.github.com>
2017-08-05 16:59:49 +0530

    Create ch.c

---
+++ ch.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+char c[25];
+printf("enter the character or letter");
+scanf("%c",&c[25]);
+char c1[10]={'a','e','i','o','u'};
+char c2[30]={'b','c','d','f','g',j','k','l','m','n','p','q',r','s','t','v','w','x','y','z'};
+if(c[10]==c1[10])
+{
+printf("it is an vowel",c[]);
+}
+elseif(c[10]==c2[30])
+{
+printf("it is an consontant",c1[]);
+}
+else
+printf("");
+}
+
