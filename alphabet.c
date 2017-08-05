bb766d9ccffe6cb859616eb0d5a83298af242f11
VSBANEES <30316990+VSBANEES@users.noreply.github.com>
2017-08-05 16:49:05 +0530

    Create alphabet.c

---
+++ alphabet.c
@@ -0,0 +1,15 @@
+include<stdio.h>
+void main()
+{
+char i;
+printf("enter the character:");
+scanf("%c",&i);
+if(i>='a' && i<=z')
+{
+printf("it is an alphabet",i);
+}
+else
+{
+printf("it is an not alphabet",i);
+}
+}
