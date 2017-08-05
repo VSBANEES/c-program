c26e580ae4cf12e684e854775ba8b6ccac15df89
VSBANEES <30316990+VSBANEES@users.noreply.github.com>
2017-08-05 16:24:08 +0530

    Create pone.c

---
+++ pone.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+int n;
+printf("enter the n value:");
+scanf("%d",&n);
+if(n>0)
+{
+printf("positive number");
+}
+elseif(n<0)
+{
+printf("negative number");
+}
+else
+{
+printf("zero");
+}
+}
