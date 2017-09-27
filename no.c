841231fb689d5a8a9be565acfa16901349a4214c
VSBANEES <30316990+VSBANEES@users.noreply.github.com>
2017-09-27 12:04:36 +0530

    Create no.c

---
+++ no.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ int a;
+ printf("enter the number");
+ scanf("%d",&a);
+ if(a>0)
+ printf("a is positive number");
+ elseif(a=0)
+ printf("a is zero");
+ else
+ printf("a is negative number");
+ return 0;
+ }
